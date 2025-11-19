d09107c3113d8462c38ea25f3c918ef7d6db297a
Samyak Borkar <samyakborkar65@gmail.com>
2025-11-19 11:05:11 +0530

    Add Footer component for collaboration board

---
+++ collabboard/frontend/src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="cb-footer">
+      <div className="cb-footer-inner">
+        <div>Fast. Simple. Real-time collaboration  - </div>
+        <br />
+        <div>  CollabBoard </div>
+      </div>
+    </footer>
+  );
+}
